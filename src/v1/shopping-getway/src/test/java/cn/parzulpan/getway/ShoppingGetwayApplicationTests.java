9b5a6e22c66ed5427b7e9e00bb92048416962853
parzulpan <parzulpan@gmail.com>
2021-01-05 20:57:13 +0800

    update: Spring Cloud Getway;

---
+++ src/v1/shopping-getway/src/test/java/cn/parzulpan/getway/ShoppingGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.parzulpan.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingGetwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
